f3da00759797c8ba14e7355e2ac39c107a116219
Scott McDonald <smcdo0211@gmail.com>
2024-03-10 22:08:23 -0400

    Combined branches

--- app/src/main/java/com/example/swiftpark/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/swiftpark/ui/home/HomeFragment.java
@@ -24,8 +24,9 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+       final TextView textView = binding.greetText;
+       homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
